68e1db8b51e7f6813a0d16ad6e330d7cc9135803
rakesh-gupta29 <rakesh.rkgupta29@gmail.com>
2023-08-02 13:13:21 +0530

    fix: react-ts error

--- react-ts/src/organisms/contact_form.tsx
+++ react-ts/src/organisms/contact_form.tsx
@@ -1,11 +1,8 @@
-import React, { Fragment, useState } from 'react'
+import React, { useState } from 'react'
 import { z } from 'zod'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Listbox, Transition } from '@headlessui/react'
 import { MasterBtn } from 'atoms/buttons'
-import { toast, ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 type FormProps = {
   isPurposeField: boolean
@@ -27,8 +24,7 @@ const formSchema = z.object({
 
 export type FormFieldSchema = z.infer<typeof formSchema>
 
-export default function Form({ isPurposeField, formTitle = '' }: FormProps) {
-  const [isTouched, setTouched] = useState(false) // avoid error while mounting the component
+export default function Form({ formTitle = '' }: FormProps) {
   const [isLoading, setLoading] = useState(false)
   const {
     register,
@@ -43,123 +39,122 @@ export default function Form({ isPurposeField, formTitle = '' }: FormProps) {
     setLoading(true)
     const res = await fetch('')
     if (res.ok) {
-      toast.success('Form has been submitted successfully.')
+      console.log(data)
+      console.log('form has been submitted')
     } else {
-      toast.error('Failed to submit the form. Please try again later.')
+      console.error('Form error')
     }
     reset()
     setLoading(false)
   }
 
   return (
-    <>
-      <section className="blade-bottom-padding-sm">
-        <section className="blade-top-padding blade-bottom-padding-lg">
+    <section className="blade-bottom-padding-sm">
+      <section className="blade-top-padding blade-bottom-padding-lg">
+        <div>
+          {formTitle && (
+            <div className="">
+              <h2
+                className="font-bold mx-auto text-center leading-normal blade-bottom-padding-sm lg:block hidden"
+                dangerouslySetInnerHTML={{ __html: formTitle }}
+              />
+              <h3
+                className="font-bold mx-auto text-center leading-normal blade-bottom-padding-sm lg:hidden block max-w-sm w-11/12 capitalize"
+                dangerouslySetInnerHTML={{ __html: formTitle }}
+              />
+            </div>
+          )}
           <div>
-            {formTitle && (
-              <div className="">
-                <h2
-                  className="font-bold mx-auto text-center leading-normal blade-bottom-padding-sm lg:block hidden"
-                  dangerouslySetInnerHTML={{ __html: formTitle }}
-                />
-                <h3
-                  className="font-bold mx-auto text-center leading-normal blade-bottom-padding-sm lg:hidden block max-w-sm w-11/12 capitalize"
-                  dangerouslySetInnerHTML={{ __html: formTitle }}
+            <form
+              noValidate
+              onSubmit={handleSubmit(onSubmit)}
+              className="max-w-screen-sm mx-auto px-3 md:w-11/12 grid  gap-4 md:gap-7"
+            >
+              <div>
+                <label
+                  htmlFor="name"
+                  className="grid gap-1 text-base md:text-lg"
+                >
+                  Name
+                  <input
+                    className=""
+                    id="name"
+                    placeholder="Hey there! Let's break the ice, shall we?"
+                    {...register('name')}
+                  />
+                  {errors.email && (
+                    <span className="">{errors.name?.message}</span>
+                  )}
+                </label>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="grid gap-1 text-base md:text-lg"
+                >
+                  Email Address
+                  <input
+                    className=""
+                    id="email"
+                    type="email"
+                    inputMode="email"
+                    placeholder="Make sure it's the one you check often."
+                    {...register('email')}
+                  />
+                  {errors.email && (
+                    <span className="">{errors.email?.message}</span>
+                  )}
+                </label>
+              </div>
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="grid gap-1 text-base md:text-lg"
+                >
+                  Contact No.
+                  <input
+                    className=""
+                    type="number"
+                    id="phone"
+                    inputMode="numeric"
+                    placeholder="Your digits are safe with us. We're too cool for cold calls anyway."
+                    {...register('phone')}
+                  />
+                  {errors.phone && (
+                    <span className="">{errors.phone?.message}</span>
+                  )}
+                </label>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="message"
+                  className="grid gap-1 text-sm md:text-lg"
+                >
+                  Message
+                  <textarea
+                    rows={7}
+                    cols={5}
+                    id="message"
+                    className=""
+                    placeholder="Got something to say? We're all ears!"
+                    {...register('message')}
+                  />
+                </label>
+              </div>
+              <div className="grid place-content-start">
+                <MasterBtn
+                  type="submit"
+                  text={isLoading ? 'Loading...' : 'Join Now'}
+                  isDisabled={isLoading}
+                  size="large"
                 />
               </div>
-            )}
-            <div>
-              <form
-                noValidate
-                onSubmit={handleSubmit(onSubmit)}
-                className="max-w-screen-sm mx-auto px-3 md:w-11/12 grid  gap-4 md:gap-7"
-              >
-                <div>
-                  <label
-                    htmlFor="name"
-                    className="grid gap-1 text-base md:text-lg"
-                  >
-                    Name
-                    <input
-                      className=""
-                      id="name"
-                      placeholder="Hey there! Let's break the ice, shall we?"
-                      {...register('name')}
-                    />
-                    {errors.email && (
-                      <span className="">{errors.name?.message}</span>
-                    )}
-                  </label>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="grid gap-1 text-base md:text-lg"
-                  >
-                    Email Address
-                    <input
-                      className=""
-                      id="email"
-                      type="email"
-                      inputMode="email"
-                      placeholder="Make sure it's the one you check often."
-                      {...register('email')}
-                    />
-                    {errors.email && (
-                      <span className="">{errors.email?.message}</span>
-                    )}
-                  </label>
-                </div>
-                <div>
-                  <label
-                    htmlFor="phone"
-                    className="grid gap-1 text-base md:text-lg"
-                  >
-                    Contact No.
-                    <input
-                      className=""
-                      type="number"
-                      id="phone"
-                      inputMode="numeric"
-                      placeholder="Your digits are safe with us. We're too cool for cold calls anyway."
-                      {...register('phone')}
-                    />
-                    {errors.phone && (
-                      <span className="">{errors.phone?.message}</span>
-                    )}
-                  </label>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="message"
-                    className="grid gap-1 text-sm md:text-lg"
-                  >
-                    Message
-                    <textarea
-                      rows={7}
-                      cols={5}
-                      id="message"
-                      className=""
-                      placeholder="Got something to say? We're all ears!"
-                      {...register('message')}
-                    />
-                  </label>
-                </div>
-                <div className="grid place-content-start">
-                  <MasterBtn
-                    type="submit"
-                    text={isLoading ? 'Loading...' : 'Join Now'}
-                    isDisabled={isLoading}
-                    size="large"
-                  />
-                </div>
-              </form>
-            </div>
+            </form>
           </div>
-        </section>
+        </div>
       </section>
-    </>
+    </section>
   )
 }
--- react-ts/src/organisms/footer.tsx
+++ react-ts/src/organisms/footer.tsx
@@ -1,6 +1,4 @@
-import { NavLink } from 'react-router-dom'
-import logo from 'assets/navbar/logo.svg'
-import { TextAnchor } from 'atoms/links'
+import React from 'react'
 
 export default function Footer() {
   return <footer>welcome from footer</footer>
